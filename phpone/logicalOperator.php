6413131a14ee524870f0b0277eafb56c02f1fa1d
sam <295044687@qq.com>
2019-03-05 01:01:33 +0800

    【 逻辑运算符 】 （ && 逻辑 与 ）（ || 逻辑 或 ）

--- phpone/logicalOperator.php
+++ phpone/logicalOperator.php
@@ -29,3 +29,17 @@ if($int%3==0 || $int%5==0){
 }else{
 	echo "{$int} 不可以被3和5整除";
 }
+
+
+// ! 逻辑非 对比较或者判断的结果取反
+//
+//注意问题：需要将取反的对象使用()包裹，防止取反出现错误
+// !(5 > 3); 对5大于3的结果取反
+// !5>3 对5取反，比较是否大于3
+
+
+// 逻辑异或 x0r  两个结果相同，结果是false，结果不同返回true
+// 
+var_dump(true xor true);
+var_dump(false xor false);
+var_dump(false xor true);

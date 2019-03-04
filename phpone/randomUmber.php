a3f3fd8f295b47ab614b2087a6ef8e70850e6c85
mingshanliu <liumingshan09@gmail.com>
2019-03-04 16:57:16 +0800

    生成随机颜色   <?php echo mt_rand();?>

--- phpone/randomUmber.php
+++ phpone/randomUmber.php
@@ -2,4 +2,19 @@
 
 // 生成随机颜色
 // 使用rgb()形成的颜色设置
-//数值是0-255之间的数字，使用php程序随机生成字体颜色
+// 数值是0-255之间的数字，使用php程序随机生成字体颜色
+/*
+// <?php echo mt_rand();?>
+*/
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<span style="color:rgb(<?php echo mt_rand(0,255);?>,<?php echo mt_rand(0,255);?>,<?php echo mt_rand(0,255);?>);">深</span>
+	<span style="color:rgb(<?php echo mt_rand(0,255);?>,<?php echo mt_rand(0,255);?>,<?php echo mt_rand(0,255);?>);">圳</span>
+</body>
+</html>

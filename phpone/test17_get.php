9675c6c2ea0354480d132781db51b7046e3c1768
sam <295044687@qq.com>
2019-03-07 00:22:55 +0800

    【 预定义变量 】  $_GET

--- phpone/test17_get.php
+++ phpone/test17_get.php
@@ -9,5 +9,28 @@ echo '</pre>';
 
 $int1 = $_GET['name'];
 $int2 = $_GET['pwd'];
+$type = $_GET['type'];
 
-echo $int1,' ',$int2;
+echo $int1,' ',$int2,' ',$type;
+
+echo "<hr>";
+
+//需要更具数据来执行计算
+if( $type == 0 ){
+	// 需要执行两个数据的加法
+	echo $int1 , '+' , $int2 , '=' , $int1 + $int2;
+}elseif($type == 1){
+	echo $int1, '-' , $int2, '=', $int1 - $int2;
+}
+elseif($type == 2){
+	echo $int1, '*' , $int2, '=', $int1 * $int2;
+}
+elseif($type == 3){
+	if($int2 == 0){
+		// 除数如果为0，需要终止程序
+		// 使用 die()函数 或 exit()
+		// 作用：终止程序，并且输出小括号中的内容
+		die('除数不能为0，请重新输入');
+	}
+	echo $int1, '/' , $int2, '=', $int1 / $int2;
+}
